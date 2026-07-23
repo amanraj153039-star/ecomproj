b81af1a9141f71b23b5eb958159c9967304d0df1
Your Name <you@example.com>
2026-07-23 17:28:41 +0530

    third commit

--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
-
+from django.contrib.auth import authenticate, login, logout
 @csrf_exempt
 def login_page(request):
     
